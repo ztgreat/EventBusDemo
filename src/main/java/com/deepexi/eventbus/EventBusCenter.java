e73324c24042e993fb0e79aa20e6cb6e1bd6a605
张腾 <zhangteng@deepexi.com>
2019-05-23 13:05:34 +0800

    test eventbus

---
+++ src/main/java/com/deepexi/eventbus/EventBusCenter.java
@@ -0,0 +1,30 @@
+package com.deepexi.eventbus;
+
+import com.google.common.eventbus.EventBus;
+
+public class EventBusCenter {
+
+    private static EventBus eventBus = new EventBus();
+
+    private EventBusCenter() {
+
+    }
+
+    public static EventBus getInstance() {
+        return eventBus;
+    }
+
+    public static void register(Object obj) {
+        eventBus.register(obj);
+    }
+
+    public static void unregister(Object obj) {
+        eventBus.unregister(obj);
+    }
+
+    public static void post(Object obj) {
+        eventBus.post(obj);
+    }
+
+
+}
