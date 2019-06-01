d7ee8489381db48843626b9fb32b7c25c9f3df8e
ztgreat <1170881778@qq.com>
2019-06-01 18:50:33 +0800

    实例

--- src/test/java/com/deepexi/eventbus/TestEventBus.java
+++ src/test/java/com/deepexi/eventbus/TestEventBus.java
@@ -1,31 +1,37 @@
 package com.deepexi.eventbus;
 
-import com.deepexi.eventbus.observer.DataObserver1;
-import com.deepexi.eventbus.observer.DataObserver2;
+import com.deepexi.eventbus.even.OrderPaySuccessEvent;
+import com.deepexi.eventbus.even.OrderPayTimeOutEvent;
+import com.deepexi.eventbus.listener.OrderPaySuccessListener;
+import com.deepexi.eventbus.listener.OrderPayTimeOutListener;
+import com.google.common.util.concurrent.MoreExecutors;
 
 public class TestEventBus {
 
     public static void main(String[] args) throws InterruptedException {
 
-        DataObserver1 observer1 = new DataObserver1();
-        DataObserver2 observer2 = new DataObserver2();
+        OrderPaySuccessListener successListener = new OrderPaySuccessListener();
+        OrderPayTimeOutListener timeOutListener = new OrderPayTimeOutListener();
 
-        EventBusCenter.register(observer1);
-        EventBusCenter.register(observer2);
+        EventBusCenter.register(successListener);
+        EventBusCenter.register(timeOutListener);
+        System.out.println("============   支付成功事件  ====================");
 
-        System.out.println("============   start  ====================");
+        //支付成功事件
+        OrderPaySuccessEvent.OrderPaySuccessEventBuilder successEventBuilder = OrderPaySuccessEvent.OrderPaySuccessEventBuilder.anOrderPaySuccessEvent();
+        successEventBuilder.orderNumber("124").productId("3456");
 
-        // 只有注册的参数类型为String的方法会被调用
-        EventBusCenter.post("post string method");
-        EventBusCenter.post(123);
+        EventBusCenter.post(successEventBuilder.build());
 
-        System.out.println("============ after unregister ============");
-        // 注销observer2
-        EventBusCenter.unregister(observer2);
-        EventBusCenter.post("post string method");
-        EventBusCenter.post(123);
+        System.out.println("============   支付超时事件  ====================");
 
-        System.out.println("============    end           =============");
+
+        //支付超时事件
+        OrderPayTimeOutEvent.OrderPayTimeOutEventBuilder timeOutEventBuilder= OrderPayTimeOutEvent.OrderPayTimeOutEventBuilder.anOrderPayTimeOutEvent();
+        timeOutEventBuilder.orderNumber("124").productId("3456");
+
+        EventBusCenter.post(timeOutEventBuilder.build());
+
     }
 
 }
